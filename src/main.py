917c95355df8f3b46905c9656fb5b174afd0758d
dmilanop <dmilanop28@gmail.com>
2022-01-06 00:41:14 +0000

    Primer commit, guardando cambios hechos

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import requests
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -25,6 +26,11 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+def swapi_to_localhost(swapi_url):
+    return swapi_url.replace("https://www.swapi.tech/api/", "https://3000-gold-canidae-6ovungq4.ws-us25.gitpod.io/")
+
+
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
@@ -38,6 +44,45 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+
+
+# esta ruta trae todos los planetas
+@app.route('/planets', methods=['GET'])
+def handle_planet():
+    # consultar la api de Star Wars
+    response = requests.get('https://www.swapi.tech/api/planets?page=1&limit=1000')
+    response = response.json()
+
+    results = response['results']
+
+    for result in results:
+        
+        result.update(url= swapi_to_localhost(result["url"]))
+
+    return jsonify(results), 200
+
+
+@app.route('/favorites/<string:nature>', methods=['POST'])
+def handle_favorites(nature):
+    uid = request.json["uid"]
+    name = request.json["name"]
+    user_id = request.json["user_id"]
+    new_favorite = Favorite(
+        user_id = user_id,
+        name = name,
+        url = f"https://www.swapi.tech/api/{nature}/{uid}" 
+    )
+    db.session.add(new_favorite)
+    try:
+        db.session.commit()
+        return jsonify(new_favorite.serialize()), 201
+    except Exception as error:
+        db.session.rollback()
+        return jsonify(error.args), 500
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,15 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(60), nullable=False, unique=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    password = db.Column(db.String(100), unique=False, nullable=False)
+    favorites = db.relationship("Favorite", backref="user", uselist=True)
 
     def serialize(self):
         return {
@@ -17,3 +16,67 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    url = db.Column(db.String(150), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    __table_args__ = (db.UniqueConstraint(
+        "user_id",
+        "url",
+        name = "unique_favorite_user"
+    ),)
+
+#     # people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+#     # planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+#     # vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "id": self.id,
+        }
+
+
+# class People(db.Model):
+#     name = db.Column(db.String(80), nullable=False)
+#     id = db.Column(db.Integer, primary_key=True)
+#     age = db.Column(db.Integer, unique=True)
+#     favorites = db.relationship("Favorites")
+
+#     def serialize(self):
+#         return {
+#             "name": self.name,
+#             "id": self.id,
+#             "age": self.age
+#         }
+
+
+# class Planet(db.Model):
+#     name = db.Column(db.String(80), nullable=False)
+#     id = db.Column(db.Integer, primary_key=True)
+#     population = db.Column(db.Integer, nullable=False)
+#     favorites = db.relationship("Favorites")
+
+#     def serialize(self):
+#         return {
+#             "name": self.name,
+#             "id": self.id,
+#             "population": self.population
+#         }
+
+
+# class Vehicle(db.Model):
+#     name = db.Column(db.String(80), nullable=False )
+#     id = db.Column(db.Integer, primary_key=True)
+#     speed = db.Column(db.Integer, nullable=False)
+#     favorites = db.relationship("Favorites")
+
+#     def serialize(self):
+#         return {
+#             "name": self.name,
+#             "id": self.id,
+#             "speed": self.speed
+#         }
